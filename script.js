f8746eb3d7f15c6f943eab1a0688fa144ec5f31a
Hepatica <danil.tkachenko@getmore.cz>
2024-08-31 03:32:22 +0200

    Add images and charity as well about section

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const gear = document.querySelector('.gear');
+const scrollIndicator = document.querySelector('.scroll-indicator');
 
 const audio = new Audio('gear.mp3');
-
 
 audio.volume = 0.1;
 
 let isPlaying = false;
 
 gear.addEventListener('click', () => {
-    isPlaying = !isPlaying;
     audio[isPlaying ? 'pause' : 'play']();
+    isPlaying = !isPlaying;
 })
+
+const onScroll = () => {
+  gear.style.left = window.scrollY > 50 ? '-120px' : 'calc(33% - 50px)';
+  scrollIndicator.style.width =`${(document.documentElement.scrollTop /
+    (document.documentElement.scrollHeight -
+      document.documentElement.clientHeight)) *
+    document.documentElement.clientWidth}px`
+}
+
+onScroll();
+window.addEventListener('scroll', onScroll);
+window.addEventListener('resize', onScroll);
+
+
+
